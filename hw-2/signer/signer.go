626771ba4ae70f309e9c0c5f8b36751475eff1d3
Denactive <d.turchin@bk.ru>
2021-09-12 18:54:51 +0300

    pipeline done

--- hw-2/signer/signer.go
+++ hw-2/signer/signer.go
@@ -1,3 +1,38 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const PIPELINE_DEBUG = true
+
+func ExecutePipeline(jobs ...job) {
+	wg := new(sync.WaitGroup)
+	var prev_chan chan interface{} = nil
+
+	for i, job_i := range jobs {
+		cur_chan := make(chan interface{}, 1)
+
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, job_i job, i int, in, out chan interface{}) {
+			defer wg.Done()
+
+			if PIPELINE_DEBUG {
+				fmt.Println("#", i, job_i, "started. input", in, ", output", out)
+			}
+
+			job_i(in, out)
+			time.Sleep(time.Second)
+			close(out)
+
+			if PIPELINE_DEBUG {
+				fmt.Println("#", i, job_i, "finished. Channel", out, "closed")
+			}
+		}(wg, job_i, i, prev_chan, cur_chan)
+
+		prev_chan = cur_chan
+	}
+	wg.Wait()
+}
